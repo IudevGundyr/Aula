239b451dda7fc8ee5637786745966a06687713cc
IudevGundyr <90079225+IudevGundyr@users.noreply.github.com>
2023-08-20 21:07:36 -0300

    a

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
 
+    private static readonly float DestructionThreshold = -10f;
+
     private Rigidbody myRB;
 
     public Vector2 force = new Vector2(10, 10);
@@ -14,20 +16,22 @@ public class Ball : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var forceAmountY = Random.Range(force.x, force.y);
-        var forceAmountX = forceAmountY = Random.Range(-0.5f, 0.5f);
-        var forceAmountZ = forceAmountY = Random.Range(-0.5f, 0.5f);
-
-
+        float forceAmountY = Random.Range(force.x, force.y);
+        float forceAmountX = forceAmountY = Random.Range(-0.5f, 0.5f);
+        float forceAmountZ = forceAmountY = Random.Range(-0.5f, 0.5f);
+        Vector3 forceVector = new Vector3(forceAmountX, forceAmountY, forceAmountZ);
 
         myRB = gameObject.GetComponent<Rigidbody>();
-        var forceVector = new Vector3(forceAmountX, forceAmountY, forceAmountZ);
         myRB.AddForce(forceVector, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float y = transform.position.y;
+        if(y <= DestructionThreshold)
+        {
+            Destroy(gameObject);
+        }
     }
 }
